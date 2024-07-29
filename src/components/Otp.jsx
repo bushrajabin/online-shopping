8fe27537d4e996c9e5fd937fa2ce14b23ca00d1d
Bushra jabin <bushrabintjabin@gmail.com>
2024-07-29 15:24:00 +0530

    Added some changes

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -3,17 +3,39 @@ import OTPInput from "react-otp-input";
 
 function Otp() {
   const [otp, setOtp] = useState("");
+  // const [confirmationResult, setConfirmationResult] = useState(null);
+
+  
+  const verifyOTP = async (otp) => {
+    if (!confirmationResult) {
+      alert("erroor")
+      return;
+    }
+
+    try {
+      const result = await confirmationResult.confirm(otp);
+      console.log(result);
+      console.log("User signed in successfully:", result.user);
+      // Navigate to another page or update the UI as necessary
+    } catch (error) {
+      console.error("Error during OTP verification:", error);
+      setError("Invalid OTP. Please try again.");
+    }
+  };
   return (
     <>
-      <div>
+      <div className="bg-red-900 text-black ">
         <h2>Otp</h2>
-      <OTPInput
-        value={otp}
-        onChange={setOtp}
-        numInputs={4}
-        // renderSeparator={<span>-</span>}
-        renderInput={(props) => <input {...props} />}
-      />
+        <OTPInput
+          value={otp}
+          onChange={(e)=>setOtp(e.target.value)}
+          numInputs={6}
+          // renderSeparator={<span>-</span>}
+          renderInput={(props) => <input {...props} />}
+          className=" xl:text-black xl:bg-red-800"
+        />
+
+        <button onClick={verifyOTP}>VerifyOTp</button>
       </div>
     </>
   );
--- src/components/PhoneAuth.jsx
+++ src/components/PhoneAuth.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../firebase/Firebase"; // Ensure this is correctly set up
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const PhoneAuth = () => {
+  const navigate = useNavigate();
   const [mynumber, setMynumber] = useState("+91");
   const [error, setError] = useState("");
   const [isCodeSent, setIsCodeSent] = useState(false);
-  const [confirmationResult, setConfirmationResult] = useState(null);
+  // const [confirmationResult, setConfirmationResult] = useState(null);
+
+  console.log(isCodeSent);
 
   const appVerifier = window.recaptchaVerifier;
 
@@ -40,6 +44,7 @@ const PhoneAuth = () => {
         console.log("result ", result);
         alert("code sent", result);
         setIsCodeSent(true);
+        // navigate('/otp')
       })
       .catch((err) => {
         console.log("err", err);
@@ -48,12 +53,13 @@ const PhoneAuth = () => {
 
   const verifyOTP = async (otp) => {
     if (!confirmationResult) {
-      setError("No OTP sent.");
+      alert("erroor");
       return;
     }
 
     try {
       const result = await confirmationResult.confirm(otp);
+      console.log(result);
       console.log("User signed in successfully:", result.user);
       // Navigate to another page or update the UI as necessary
     } catch (error) {
@@ -67,10 +73,8 @@ const PhoneAuth = () => {
       <h1 className="text-lg font-bold mb-4">Login with Phone Number</h1>
 
       <div className="bg-white p-5 flex flex-col gap-2 items-center rounded-lg shadow-lg">
-        <div id="recaptcha-container" className="id"></div>
         {error && <p className="text-red-500  my-3 text-xs ">{error}</p>}
         {/* Add input for OTP and a button to verify */}
-
         {isCodeSent ? (
           <>
             <input
@@ -90,6 +94,8 @@ const PhoneAuth = () => {
           </>
         ) : (
           <>
+                  <div id="recaptcha-container" className="id"></div>
+
             <input
               type="tel"
               placeholder="Enter your phone number"
