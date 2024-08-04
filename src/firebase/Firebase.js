e0aad370747f0d777403889b435b68ee76cb9ce1
Bushra jabin <bushrabintjabin@gmail.com>
2024-08-04 21:32:55 +0530

    Added env file:

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: import.meta.env.VITE_STORAGEBUCKET,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASURMENTID,
 };
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
