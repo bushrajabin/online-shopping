5004a572fd6ce5e43ccb140d281836cb20455db1
Bushra jabin <bushrabintjabin@gmail.com>
2024-08-04 21:13:23 +0530

    update env file

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -14,12 +14,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC5IPxTzWdRd3PMOqpvfI459-O5eWKS1nQ",
   authDomain: "phone-auth-252c2.firebaseapp.com",
