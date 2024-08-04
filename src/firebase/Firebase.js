4feabe258b1d99ae4a6a40e9d0a0a045daf1f423
Bushra jabin <bushrabintjabin@gmail.com>
2024-08-04 21:46:04 +0530

    Added env file:

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_STORAGEBUCKET,
+  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
